32786b1e634626041bfdd5402d5511aa70b1b9b6
Promodemia <39663044+Promodemia@users.noreply.github.com>
2023-10-28 00:33:11 +0500

    Add files via upload

---
+++ schemas.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from typing import Union
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class ProductBase(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+
+
+class ProductCreate(ProductBase):
+    pass
+
+
+class ProductUpdate(ProductBase):
+    pass
+
+
+class Product(ProductBase):
+    id: int
+    datetime: datetime
